4125a52df34a5710da674c9cb1f9c69963e12040
DONELL ELLIS <DONELL.ELLIS@GMAIL.COM>
2023-03-23 13:36:33 -0700

    Added QueryClientProvider and ReactQueryDevtools to App. Setup pagination and pre-fetched data in Posts. Implement useMutation hook for deleting and updating posts in PostDetail, conditionaly rendering copy based on isError, isloading, and isSuccess states.

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useQuery, useQueryClient } from "react-query";
 
 import { PostDetail } from "./PostDetail";
-const maxPostPage = 10;
+const maxPostPage = 9;
 
-async function fetchPosts() {
+async function fetchPosts(pageNum) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
   );
   return response.json();
 }
@@ -14,8 +15,35 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
+  const queryClient = useQueryClient();
+
+  // run function anytiime currentPage change
+  useEffect(() => {
+    if (currentPage < maxPostPage) {
+      const nextPage = currentPage + 1;
+      queryClient.prefetchQuery(["posts", nextPage], () =>
+        fetchPosts(nextPage)
+      );
+    }
+  }, [currentPage, queryClient]);
+
   // replace with useQuery
-  const data = [];
+  // first argument is the query key, or name of the query
+  // second arguments needs to be an async function that fetches the data
+  // fetchPosts is asynchronous, so it will return undefined until it resolves
+  const { data, isError, error, isLoading } = useQuery(
+    ["posts", currentPage],
+    () => fetchPosts(currentPage),
+    { staleTime: 2000, keeppreviousData: true }
+  );
+  if (isLoading) return <h3>Loading...</h3>;
+  if (isError)
+    return (
+      <>
+        <h3>Oops, somethign went wrong</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
 
   return (
     <>
@@ -31,11 +59,21 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 0}
+          onClick={() => {
+            setCurrentPage((previousValue) => previousValue - 1);
+          }}
+        >
           Previous page
         </button>
         <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage((previousValue) => previousValue + 1);
+          }}
+        >
           Next page
         </button>
       </div>
